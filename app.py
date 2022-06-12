8c984ad43fdbbf800ad63b164afd831f204a2460
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-06-12 22:40:12 +0000

    .

--- app.py
+++ app.py
@@ -18,11 +18,28 @@ def find_coordinate(address_name):
 # Creating a function to load coordinates from an excel file
 def find_bulk_coordinates():
     arc = ArcGIS()
+
+    # Gathering the necessary info for operation
+
     file_name = input("What is the path of the file? -- :  ")
     sheet_no = int(input("How many sheets are in the workbook? -- :  "))
-    worksheet = int(input("which sheet number had the locations to find the coordinates? -- :  "))
-    if 
-    df = pd.read
+    ps_name = input("What name have you stored the polling station names column? -- :  ")
+    region = input("Which region are you dealing with? -- :  ")
+    constituency = input("How have you stored the constituency column? -- :  ")
+
+    # Putting up the conditions
+
+    if sheet_no == 1:
+        df = pd.read_excel(file_name, sheet_name=0)
+    elif sheet_no > 1:
+        worksheet = int(input("which sheet number has the locations to find the coordinates? -- :  "))
+        df = pd.read_excel(file_name, sheet_name=worksheet-1)
+    else:
+        print("Workbook empty, Provide worksheet to work on!")
+        quit()
+
+    address = df[ps_name] + df[region] + df[constituency]
+
 
 
 
